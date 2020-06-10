a5ffcde90891dfa01d0aeb8472a141bee1ccbbdc
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-06-10 21:11:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each do |character|
-    puts array.each_with_index(character)
+    puts character.each_with_index(character)
   end
   # Use `each_with_index` to enumerate over the provided array
   #
